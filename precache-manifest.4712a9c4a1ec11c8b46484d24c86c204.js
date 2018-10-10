8c966eb96ada95dbaa8ed7ff675394a27c0fc167
Jorge Abad <jorgejoseabad@gmail.com>
2018-10-10 10:00:16 +0200

    Updates

---
+++ precache-manifest.4712a9c4a1ec11c8b46484d24c86c204.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b215c30b9ad3e8253b24",
+    "url": "/Tutorial-react/static/js/runtime~main.b215c30b.js"
+  },
+  {
+    "revision": "b5e0f011fc967f870c53",
+    "url": "/Tutorial-react/static/js/main.b5e0f011.chunk.js"
+  },
+  {
+    "revision": "855276873adeae1ea0f1",
+    "url": "/Tutorial-react/static/js/1.85527687.chunk.js"
+  },
+  {
+    "revision": "b5e0f011fc967f870c53",
+    "url": "/Tutorial-react/static/css/main.582b37ed.chunk.css"
+  },
+  {
+    "revision": "e738edd6a994568ef39e1229fbba0b1c",
+    "url": "/Tutorial-react/index.html"
+  }
+];
